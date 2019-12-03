0ba9c784401b8d379ae3672fbfbccda604a94e4c
WeiLai <futuresa@126.com>
2019-12-03 16:23:54 +0800

    添加高龄津贴页面；

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -12,10 +12,10 @@
 
             <el-submenu index="">
                 <template slot="title">我要申请</template>
-                <el-menu-item index="test">城市最低生活保障金</el-menu-item>
+                <el-menu-item index="3-1">城市最低生活保障金</el-menu-item>
                 <el-menu-item index="3-2">残疾人保障金</el-menu-item>
                 <el-menu-item index="3-3">大学生创业吸纳就业奖励</el-menu-item>
-                <el-menu-item index="3-4">高龄津贴</el-menu-item>
+                <el-menu-item index="ageAllowance">高龄津贴</el-menu-item>
                 <el-menu-item index="3-5">分散孤儿价格临时补贴</el-menu-item>
             </el-submenu>
 
---
+++ src/views/AgeAllowance.vue
@@ -0,0 +1,146 @@
+<template>
+<div class="mainPage">
+    <div class="currentLocation">当前位置：首页 > 最新服务 > 高龄津贴</div>
+    </br>
+    <div class="first-inline">
+        <div class="line"><div class="redTag"></div>高龄津贴</div>
+        <div class="applyBtn">
+            <!-- Form -->
+            <el-button type="text" @click="dialogFormVisible = true" class="tipsTitle">在线申请入口</el-button>
+            
+            <el-dialog title="" :visible.sync="dialogFormVisible">
+            <div class="applyHead">
+                <img class="applyLogo" src="../assets/logo.jpg"/>
+                <span class="applyTileText"><span class="firstText"> | 政务一卡通 </span>| 高龄津贴申请</span>
+             </div>
+                <el-form :model="form">
+                    
+                    <el-input v-model="form.name" autocomplete="off" type='hidden'></el-input>
+                    
+                    <el-form-item class="content">
+                    <span class="labalTips">资料上传 :</span>
+                    
+                        <el-upload
+                            class="upload-demo"
+                            action="https://jsonplaceholder.typicode.com/posts/"
+                            :on-preview="handlePreview"
+                            :on-remove="handleRemove"
+                            :before-remove="beforeRemove">
+                            <el-button size="small" type="primary">点击上传</el-button>
+                        </el-upload>
+                    
+                    </el-form-item>
+                </el-form>
+                <div slot="footer" class="dialog-footer" >
+                    <el-button type="primary" @click="dialogFormVisible = false">提交申请</el-button>
+                    <el-button @click="dialogFormVisible = false">取 消</el-button>
+                </div>
+            </el-dialog>
+
+        </div>
+    </div>
+    <div class="applyProcess">
+        <el-tag class="tipsTitle">申请流程</el-tag>
+    </div>
+    <div class="downloadFiles">
+        <el-tag class="tipsTitle">材料下载</el-tag>
+    </div>
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  components: {
+
+  },
+    data() {
+      return {
+        dialogTableVisible: false,
+        dialogFormVisible: false,
+        form: {
+          
+        },
+        formLabelWidth: '120px'
+      };
+    },
+    methods: {
+      handleRemove(file, fileList) {
+        console.log(file, fileList);
+      },
+      handlePreview(file) {
+        console.log(file);
+      },
+      beforeRemove(file, fileList) {
+        return this.$confirm(`确定移除 ${ file.name }？`);
+      }
+    }
+}
+
+
+</script>
+
+<style lang="scss">
+.mainPage{
+    margin-left:50px;
+}
+.currentLocation{
+    font-size:12px;
+}
+.line{
+    display:inline-block;
+    display:-webkit-inline-box;
+    font-size:20px;
+}
+.redTag{
+    width:10px;
+    height:30px;
+    background-color:#E8313e;
+    margin-right:10px
+}
+
+.first-inline{
+    display:inline-block;
+    display:-webkit-inline-box;
+}
+.applyBtn{
+   margin-left:400%;
+}
+.tipsTitle{
+   
+    color:white;
+    background-color:#E8313e;
+}
+
+.applyLogo{
+    width:20%;
+    height:20%;
+    vertical-align: middle;
+
+}
+.applyTileText{
+    margin-top:-10px;
+    font-size:15px;
+}
+.firstText{
+    font-size:18px;
+    font-weight: 600;
+}
+
+.labalTips{
+    
+    font-size:15px;
+    font-weight:700;
+}
+.content{
+    margin-top:10px;
+    margin-left: 20px;
+}
+/*.upload-demo{
+    display:inline-block;
+    display:-webkit-inline-box;
+    margin-left:20px
+}*/
+</style>
